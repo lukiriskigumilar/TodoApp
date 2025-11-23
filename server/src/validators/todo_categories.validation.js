e2486c381d9382b43547c1bda0c7efc7fe9c20a0
Luki Riski <contactlukiriski@gmail.com>
2025-11-24 00:25:14 +0700

    chore(backend): add todo categories schema

---
+++ server/src/validators/todo_categories.validation.js
@@ -0,0 +1,6 @@
+import Joi from "joi";
+
+export const todoCategoriesSchema = Joi.object({
+    user_id:Joi.string().min(3).max(50).pattern(/^[A-Za-z0-9@._-]+$/).required(),
+    name:Joi.string().min(3).max(50).pattern(/^[A-Za-z0-9@._-]+$/).required(),
+})
