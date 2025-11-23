90af2a09f758c8bea8b9459de5843e6729dd27f9
Luki Riski <contactlukiriski@gmail.com>
2025-11-23 18:58:15 +0700

    feat(backend): add route user module

--- server/src/routes/api.v1.js
+++ server/src/routes/api.v1.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import userRouter from '../modules/users/user.route.js';
 
 
 const router = express.Router();
 
-
+router.use('/user', userRouter)
 
 export default router
 
