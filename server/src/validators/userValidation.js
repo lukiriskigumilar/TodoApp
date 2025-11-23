5475042b1dc596fc9d0cf691215c0ad810c6efc6
Luki Riski <contactlukiriski@gmail.com>
2025-11-23 19:09:33 +0700

    chore(backend): add regex pattern

--- server/src/validators/userValidation.js
+++ server/src/validators/userValidation.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-    username:Joi.string().min(3).max(50).required(),
+    username:Joi.string().min(3).max(50).pattern(/^[A-Za-z0-9@._-]+$/).required(),
 })
