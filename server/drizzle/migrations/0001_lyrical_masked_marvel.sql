c1bb16d540158477212065ed544aa06b0f718939
Luki Riski <contactlukiriski@gmail.com>
2025-11-23 19:35:11 +0700

    chore(backend): add new migration for adding field user_id in table todo_categories

---
+++ server/drizzle/migrations/0001_lyrical_masked_marvel.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "todo_categories" ADD COLUMN "user_id" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "todo_categories" ADD CONSTRAINT "todo_categories_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
