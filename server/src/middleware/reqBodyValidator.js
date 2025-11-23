3dfa2180eb64ff3c710f75f715578ede13929a9b
Luki Riski <contactlukiriski@gmail.com>
2025-11-23 15:30:30 +0700

    chore(backend): add request body validator middleware

---
+++ server/src/middleware/reqBodyValidator.js
@@ -0,0 +1,25 @@
+import AppError from "../helpers/appError.js";
+
+const reqBodyValidator = (schema) => {
+    return (req,res,next) => {
+        const contentType = req.headers['content-type'];
+        if(!contentType|| !contentType.includes('application/json')){
+        return next(
+           new AppError("Unsupported Media Type. Please use application/json", 415, [])
+         )
+        }
+
+        const {error} = schema.validate(req.body, {abortEarly: false});
+        if(error){
+            const errors = error.details.map(detail => detail.message)
+          return next(
+                new AppError(
+                "validation errors",
+                400,
+                errors
+            )
+            )
+        }
+        next()
+    }   
+}
