df24806d6ab8b663228ac0b0368321ea5c354a1c
Luki Riski <contactlukiriski@gmail.com>
2025-11-24 13:12:02 +0700

    refactor(backend): clean up createTodosCategory service and extract assertUserExist helper

--- server/src/modules/todo_categories/todo_categories.service.js
+++ server/src/modules/todo_categories/todo_categories.service.js
@@ -2,34 +2,44 @@ import todo_categoriesRepository from "./todo_categories.repository.js";
 import AppError from "../../helpers/appError.js";
 import { v4 as uuidV4 } from "uuid";
 
-
-
-const createTodoCategoriesService = async (data)=>{   
-    
-    const checkRelationUser = await todo_categoriesRepository.findUserById(data.user_id)
-    if(!checkRelationUser){
-        throw new AppError('failed insert todo categories',
-            404,
-            {
-                reason:"user id not valid"
-            }
-        )
-    }
-    
-    const existsCategory = await todo_categoriesRepository.findTodosCategoriesByName(data.name);
-    if(existsCategory){
-        throw new AppError('failed insert todo categories',
-            400,
-                {
-                    reason:"todo categories name was exist"
-                }
-           
-        )
-    }
-    const id = uuidV4();
-    data.id = id;
-    data.userId = data.user_id
-    return await todo_categoriesRepository.createTodosCategory(data);
+//----------------------------
+// SERVICE HELPER
+//----------------------------
+async function assertUserExist(userId, messageError) {
+  const user = await todo_categoriesRepository.findUserById(userId)
+  console.log(user)
+  console.log(userId)
+  if (!user) {
+    throw new AppError(messageError, 404, {
+      reason: "invalid user id",
+    });
+  }
+  return user;
 }
 
-export default{createTodoCategoriesService}
+//------------------------
+//SERVICE
+//------------------------
+const createTodoCategoriesService = async (data) => {
+  const userId = data.user_id;
+  await assertUserExist(userId, "Create todo category failed");
+
+  const existsCategory =
+    await todo_categoriesRepository.findTodosCategoriesByName(data.name);
+
+  if (existsCategory) {
+    throw new AppError("failed insert todo categories", 400, {
+      reason: "todo categories name was exist",
+    });
+  }
+  
+  const payload = {
+    id:uuidV4(),
+    name:data.name,
+    userId
+  }
+
+  return await todo_categoriesRepository.createTodosCategory(payload);
+};
+
+export default { createTodoCategoriesService };
