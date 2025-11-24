1025d12e056950d62d1bc9308c1738957eca8177
Luki Riski <contactlukiriski@gmail.com>
2025-11-24 09:04:31 +0700

    feat(backend): add route todo categories module

--- server/src/routes/api.v1.js
+++ server/src/routes/api.v1.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userRouter from '../modules/users/user.route.js';
+import todoCategoriesRoute from '../modules/todo_categories/todo_categories.route.js';
 
 
 const router = express.Router();
 
 router.use('/user', userRouter)
+router.use('/todo-categories', todoCategoriesRoute)
 
 export default router
 
