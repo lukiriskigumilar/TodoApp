5ad5828fd816396ba679d1bec09b5bad0c06208f
Luki Riski <contactlukiriski@gmail.com>
2025-11-24 14:54:00 +0700

    feat(backend): add get todo categories route

--- server/src/modules/todo_categories/todo_categories.route.js
+++ server/src/modules/todo_categories/todo_categories.route.js
@@ -1,9 +1,11 @@
 import express from "express"
 import reqBodyValidator from "../../middleware/reqBodyValidator.js"
-import { todoCategoriesSchema } from "../../validators/todo_categories.validation.js"
+import { todoCategoriesSchema,getTodosCategoriesSchema } from "../../validators/todo_categories.validation.js"
 import todo_categoriesController from "./todo_categories.controller.js"
 
 const todoCategoriesRoute = express.Router()
+
 todoCategoriesRoute.post('',reqBodyValidator(todoCategoriesSchema),todo_categoriesController.createTodoCategories)
+todoCategoriesRoute.get('', reqBodyValidator(getTodosCategoriesSchema), todo_categoriesController.getTodoCategories)
 
 export default todoCategoriesRoute
