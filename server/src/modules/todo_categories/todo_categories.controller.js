fbff76028b71e2b95620c975a495b5eacff15996
Luki Riski <contactlukiriski@gmail.com>
2025-11-24 09:02:19 +0700

    fear(backend): add todo categories controller

---
+++ server/src/modules/todo_categories/todo_categories.controller.js
@@ -0,0 +1,17 @@
+import todo_categoriesService from "./todo_categories.service.js";
+import sendSuccessResponse from "../../helpers/successResponse.js";
+import e from "express";
+
+
+const createTodoCategories = async (req,res,next) =>{
+    const data = req.body;
+
+    try {
+        const result= await todo_categoriesService.createTodoCategoriesService(data);
+        sendSuccessResponse(res,"create todo categories successfully",result,200,null)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export default{createTodoCategories}
